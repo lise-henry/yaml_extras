528f6bce43e0e0e61fe8556ffe1e2a5d0c73c3af
Elisabeth Henry <liz.henry@ouvaton.org>
2023-08-24 03:27:23 +0200

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,18 @@
 //! converting dotted keys to inner fiels:
 //!
 //! ```
-
+//!         let s1 = r#"
+//! compiler:
+//!     command: cargo build
+//! "#;
+//! 
+//!         let s2 = r#"
+//! compiler.command: cargo build
+//! "#;
+//!         let v1: serde_yaml::Value = serde_yaml::from_str(s1).unwrap();
+//!         let v2 = yaml_extras::restructure_from_str(&s2, true).unwrap();
+//!         assert_eq!(v1, v2);
+//! ```
 
 use thiserror::Error;
 
