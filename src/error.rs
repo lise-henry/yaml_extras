5b478be64eee94e16b58ea3653514aeb93dc30c7
Elisabeth Henry <liz.henry@ouvaton.org>
2023-08-24 05:39:20 +0200

    Move to multiple files

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("impossible to restructure YAML map: {0}")]
+    Restructure(String),
+    #[error("YAML error")]
+    Yaml(#[from] serde_yaml::Error)
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
