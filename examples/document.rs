6963c48e1cca262b9d0f5df2e57e82a94b7d9867
Elisabeth Henry <liz.henry@ouvaton.org>
2023-08-27 23:08:12 +0200

    ???

--- examples/document.rs
+++ examples/document.rs
@@ -1,30 +1,57 @@
+use serde_derive::Serialize;
+use std::default::Default;
+
+#[derive(Serialize, Default)]
+struct UserId {
+    first_name: String,
+    last_name: String,
+    public_id: String,
+    internal_id: u32,
+}
+
+#[derive(Serialize)]
+struct Preferences {
+    screen_size: [u32; 2],
+    allow_cookies: bool,
+    two_fa: bool
+}
+
+impl Default for Preferences {
+    fn default() -> Self {
+        Preferences {
+            screen_size: [1024, 768],
+            allow_cookies: false,
+            two_fa: true
+        }
+    }
+}
+
+#[derive(Serialize, Default)]
+struct User {
+    id: UserId,
+    preferences: Preferences,
+}
+
+
 fn main() {
     use yaml_extras::document::ValueType;
 
 
     let desc_yaml = r#"
-animal:
-    __description__: An animal
-    cat: The best animal
-    ant: An insect
+id:
+    __description__: Info about the users
+    public_id: Must oncly contain aphanumeric characters
+    internal_id: Must never be changed
+preferences:
+    __description__: Various user settings
+    screen_size: Width and height, in pixels, obviously
 "#;
 
-    let yaml = r#"
-foo: 42
-bar: true
-animal:
-    cat:
-        legs: 4
-        say: ["Nyaaah", "Meow", "rrrrr"]
-    ant:
-        legs: 6
-"#;
-
-    let value: serde_yaml::Value = serde_yaml::from_str(&yaml).unwrap();
+    let value = serde_yaml::to_value(&User::default()).unwrap();
     let desc: serde_yaml::Value = serde_yaml::from_str(&desc_yaml).unwrap();
     let d = yaml_extras::Documenter::new()
         .type_name(&|t| match t {
-            ValueType::Mapping | ValueType::Tagged | ValueType::List => format!(""),
+            ValueType::Mapping | ValueType::Tagged => format!(""),
             _ => ValueType::to_str(t)
         });
     let s = d.apply_value(&value, Some(&desc)).unwrap();
